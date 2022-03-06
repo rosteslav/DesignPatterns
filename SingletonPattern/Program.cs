50180c1272faa7c1cf2b50fbed67ac660fc5e777
rosteslav.yordanov <rosteslav.yordanov@gmail.com>
2022-03-06 02:36:42 +0200

    Add Singleton

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using SingletonPattern.Senders;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main()
+        {
+            Sender.Instance.Send("Hello!");
+            Sender.Instance.Send("Hello!");
+        }
+    }
+}
---
+++ SingletonPattern/Senders/Sender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SingletonPattern.Senders
+{
+    public sealed class Sender
+    {
+        private static Sender instance = null;
+        private static readonly object padlock = new object();
+
+        public static Sender Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Sender();
+                    }
+
+                    return instance;
+                }
+            }
+        }
+
+        public void Send(string message)
+        {
+            Console.WriteLine($"Sending '{message}'...");
+        }
+    }
+}
